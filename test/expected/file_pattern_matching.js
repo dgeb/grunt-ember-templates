82951603f680974a9ec8d784649d1ba9a2438ff5
GManzato <gilles.manzato@gmail.com>
2013-01-23 11:22:55 +0100

    Ugrade Handlebars to RC2

--- test/expected/file_pattern_matching.js
+++ test/expected/file_pattern_matching.js
@@ -8,11 +8,13 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 
 Ember.TEMPLATES["test/fixtures/simple"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  var buffer = '', stack1, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, hashTypes, escapeExpression=this.escapeExpression;
 
 
   data.buffer.push("<p>Hello, my name is ");
-  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},contexts:[depth0],data:data});
+  stack1 = {};
+  hashTypes = {};
+  stack1 = helpers._triageMustache.call(depth0, "name", {hash:stack1,contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data});
   data.buffer.push(escapeExpression(stack1) + ".</p>");
   return buffer;
 });
