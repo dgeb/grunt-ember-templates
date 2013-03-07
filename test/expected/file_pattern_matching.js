c05c8ca037e1608cb9af4e1cb805a7cfbca64122
Rod Afshar <rafshar@gmail.com>
2013-03-07 14:30:29 -0800

    Updated tests to match 0.4.3

--- test/expected/file_pattern_matching.js
+++ test/expected/file_pattern_matching.js
@@ -1,26 +1,33 @@
 Ember.TEMPLATES["test/fixtures/grandparent/parent/child"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   
 
 
   data.buffer.push("Should be nested.");
+  
 });
 
 Ember.TEMPLATES["test/fixtures/simple"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  var buffer = '', stack1, escapeExpression=this.escapeExpression;
+  var buffer = '', hashTypes, escapeExpression=this.escapeExpression;
 
 
   data.buffer.push("<p>Hello, my name is ");
-  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},contexts:[depth0],data:data});
-  data.buffer.push(escapeExpression(stack1) + ".</p>");
+  hashTypes = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "name", {hash:{},contexts:[depth0],types:["ID"],hashTypes:hashTypes,data:data})));
+  data.buffer.push(".</p>");
   return buffer;
+  
 });
 
 Ember.TEMPLATES["test/fixtures/text"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
   
 
 
   data.buffer.push("Basic template that does nothing.");
+  
 });
